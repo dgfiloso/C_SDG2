a6e3def4270eb3414222d90308ffb21da7d49250
dgfiloso <dgfiloso95@gmail.com>
2016-02-20 14:22:16 +0100

    Inicializacion de los arrays. Uso de punteros en las asignaciones de arrays. Inclusion de comentarios

--- Practica2/P2e/main_2e.c
+++ Practica2/P2e/main_2e.c
@@ -4,7 +4,8 @@
 #define MAX_CHAR 25
 
 int main(){
-	FILE* p_file;
+	//Variables
+	
 	char strg[MAX_PALABRAS][MAX_CHAR];
 	char temp[MAX_CHAR];
 	int i = 0;
@@ -12,6 +13,8 @@ int main(){
 	int longTotal;
 	int longTemp;
 
+	//Punteros
+	FILE* p_file;
 	char* p_strg;
 	char* p_temp;
 	int* p_lngTo;
@@ -21,29 +24,43 @@ int main(){
 	p_lngTo = &longTotal;
 	p_lngTe = &longTemp;
 
-	p_file = fopen("Entrada2e.txt", "r");
-	if(!p_file){return 1;}
+	//Inicializamos los arrays
+	for(j=0; j<MAX_PALABRAS; j++){
+		for(k=0; k<MAX_CHAR; k++){
+			*(p_strg+j+k) = "";
+		}
+	}
+	for(j=0;j<MAX_CHAR; j++){
+		*(p_temp+j)="";
+	}
+
+	p_file = fopen("Entrada2e.txt", "r");		//Abrimos el archivo de texto
+	if(!p_file){return 1;}						//Comprobamos que se ha abierto
+	
+	//Realizamos un bucle que no termina hasta que se termine el fichero de texto.
+	//En este bucle metemos las palabras en el array strg y calculamos el numero de
+	//palabras que se han introducido
 	while(1){
 		if(feof(p_file)){
 			break;
 		}
-		fscanf(p_file, "%s", strg[i]);
+		fscanf(p_file, "%s", p_strg + i);
 		i++;
 		p_lngTo++;
 	}
 
 	for(j=0; j<*p_lngTo; j++){
-		printf("%s " , strg[j]);
+		printf("%s " , *(p_strg + j));
 	}
 
 	*p_lngTe = *p_lngTo;
 
 	while(*p_lngTe > 0){
 		for(k=0; k<*p_lngTe; k++){
-			if(strcmp(strg[k],strg[k + 1])>0){
-				*p_temp = strg[k+1];
-				strg[k+1] = strg[k];
-				strg[k] = *p_temp;
+			if(strcmp(p_strg+k,p_strg+k + 1)>0){
+				*p_temp = *(p_strg+k+1);
+				*(p_strg+k+1) = *(p_strg+k);
+				*(p_strg+k) = *p_temp;
 			}
 		}
 		p_lngTe -= 1;
